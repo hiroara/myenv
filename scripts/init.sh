71d97afc9c81104cb486113bf43f4823f6578708
Arai Hiroki <hiroara62@gmail.com>
2020-12-10 18:33:36 +0900

    Support SSH configuration via env

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,4 +8,14 @@ if [ -d /tmp/init.d ]; then
   done
 fi
 
+if [ -n "$GITHUB_SSH_KEY" ]; then
+  SSH_DIR=/root/.ssh
+  echo "$GITHUB_SSH_KEY" > $SSH_DIR/github.key
+  chmod 600 $SSH_DIR/github.key
+fi
+
+if [ -n "$GIT_PROJECT" ]; then
+  git clone $GIT_PROJECT /work/project
+fi
+
 exec "$@"
